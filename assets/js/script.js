da3f3d4e22805fa72743ee44f4a1dbe0652e3f17
work ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-19 13:05:03 -0400

    updated script to fetch city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("script file connected");
+var getCityWeather = function (city) {
+     // awaiting key to be activated
+     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=/" + city + "&appid=" + "d0c4095bfc85fd893ffbd22250d010a9";
+
+     // make a request to the url
+     fetch(apiUrl).then(function (response) {
+          response.json().then(function (data) {
+               console.log(data);
+          });
+     });
+};
+
+var cityFormEl = document.querySelector("#city-form");
+var nameInputEl = document.querySelector("#city");
+
+var formSubmitHandler = function (event) {
+     event.preventDefault();
+     // get value from input element
+     var cityname = nameInputEl.value.trim();
+
+     if (cityname) {
+          getCityWeather(cityname);
+          nameInputEl.value = "";
+     } else {
+          alert("Please enter a valid city");
+     }
+     console.log(event);
+};
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
