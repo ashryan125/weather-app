9db2e8c168c13fe6852f5588d7f2a52167942d50
work ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-21 16:28:09 -0400

    display weather icon next to city and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
 var getCityWeather = function (city) {
      // awaiting key to be activated
-     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=/" + city + "&appid=" + "d0c4095bfc85fd893ffbd22250d010a9";
+     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + "d0c4095bfc85fd893ffbd22250d010a9";
 
      // make a request to the url
      fetch(apiUrl).then(function (response) {
-          response.json().then(function (data) {
-               console.log(data);
-          });
+          if (response.ok) {
+               response.json().then(function (data) {
+                    displayTemp(data, city);
+               });
+          } else {
+               alert("Error: City Name Not Found");
+          }
+     })
+     .catch(function(error) {
+          alert("Unable to connect to Weather Information");
      });
 };
 
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#city");
+
+var currentContainerEl = document.querySelector("#current-container");
+var citySearchTerm = document.querySelector("#city-search-term");
+var date = new Date();
+
+
 
 var formSubmitHandler = function (event) {
      event.preventDefault();
@@ -28,3 +41,23 @@ var formSubmitHandler = function (event) {
 };
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+
+var displayTemp = function (temp, searchTerm) {
+     console.log(temp);
+     console.log(searchTerm);
+     var icon = "http://openweathermap.org/img/wn/" + temp.weather[0].icon + "@2x.png";
+     var iconEl = document.createElement("img");
+     iconEl.setAttribute("src", icon);
+     iconEl.setAttribute("id", "title-icon");
+     // clear old content
+     currentContainerEl.textContent = "";
+
+     citySearchTerm.textContent = searchTerm + " (" + Intl.DateTimeFormat().format(date) + ")";
+     citySearchTerm.appendChild(iconEl);
+
+
+
+};
