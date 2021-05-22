fb422c3717782fddc4fe97f57a3b515371743d03
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-21 22:33:52 -0400

    changed to geolocation api for coords, passed coords into onecall api, adjusted code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,37 @@
-var getCityWeather = function (city) {
-     // awaiting key to be activated
-     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + "d0c4095bfc85fd893ffbd22250d010a9";
+var getCityLocation = function(city) {
+     var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=d0c4095bfc85fd893ffbd22250d010a9";
 
-     // make a request to the url
-     fetch(apiUrl).then(function (response) {
-          if (response.ok) {
+          // make a request to the geo url
+          fetch(geoUrl).then(function(geoResponse) {
+               if (geoResponse.ok) {
+                    return geoResponse.json();
+               } else {
+                    alert("Error: City Name Not Found");
+               }
+          })
+          .then(function(geoResponse) {
+               // create variables to hold lat and long
+               var lat = geoResponse[0].lat;
+               var long = geoResponse[0].lon;
+
+               // return fetch request to onecall api for lat and lon
+               return fetch(
+                    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+                    lat +
+                    "&lon=" +
+                    long +
+                    "&exclude=minutely,hourly,alerts&units=imperial&appid=d0c4095bfc85fd893ffbd22250d010a9"
+               );
+               
+          })
+          .then(function(response) {
                response.json().then(function (data) {
-                    displayTemp(data, city);
-               });
-          } else {
-               alert("Error: City Name Not Found");
-          }
-     })
-     .catch(function(error) {
-          alert("Unable to connect to Weather Information");
-     });
+               displayTemp(data, city);
+               })
+          })
+  
 };
+
 
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#city");
@@ -29,35 +45,64 @@ var date = new Date();
 var formSubmitHandler = function (event) {
      event.preventDefault();
      // get value from input element
-     var cityname = nameInputEl.value.trim();
+     var cityName = nameInputEl.value.trim();
 
-     if (cityname) {
-          getCityWeather(cityname);
+     if (cityName) {
+          getCityLocation(cityName);
           nameInputEl.value = "";
      } else {
           alert("Please enter a valid city");
      }
-     console.log(event);
+     // console.log(event);
 };
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
 
-
-
 var displayTemp = function (temp, searchTerm) {
-     console.log(temp);
-     console.log(searchTerm);
-     var icon = "http://openweathermap.org/img/wn/" + temp.weather[0].icon + "@2x.png";
+
+
+     // show temperature icon
+     var icon = "http://openweathermap.org/img/wn/" + temp.current.weather[0].icon + "@2x.png";
      var iconEl = document.createElement("img");
      iconEl.setAttribute("src", icon);
      iconEl.setAttribute("id", "title-icon");
+
+     // create ul container
+     var ulEl = document.createElement("ul");
+     ulEl.setAttribute("id", "current-temp-params");
+     ulEl.setAttribute("class", "list-group")
+
+     // create list for temp, wind, humidity, UV index
+     var tempLi = document.createElement("li");
+     tempLi.textContent = "Temp: " + temp.current.temp + "°F";
+
+     var windLi = document.createElement("li");
+     windLi.textContent = "Humidity: " + temp.current.humidity + " %";
+
+     var humidityLi = document.createElement("li");
+     humidityLi.textContent = "Wind: " + temp.current.wind_speed + " MPH";
+
+
+     // uv index 0-2 green "low", 3-5 yellow "moderate", 6-7 orange "high", 8-10 red "very high"
+     var uvLi = document.createElement("li");
+     uvLi.textContent = "UV Index: " + temp.current.uvi;
+
+
      // clear old content
      currentContainerEl.textContent = "";
 
      citySearchTerm.textContent = searchTerm + " (" + Intl.DateTimeFormat().format(date) + ")";
      citySearchTerm.appendChild(iconEl);
+     
+     currentContainerEl.appendChild(ulEl);
 
+     ulEl.appendChild(tempLi);
+     ulEl.appendChild(windLi);
+     ulEl.appendChild(humidityLi);
+     ulEl.appendChild(uvLi);
+
+     console.log(temp);
 
 
 };
